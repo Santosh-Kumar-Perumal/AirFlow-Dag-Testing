adc8292a54984d469d85d88fd8aa0b2bb961241e
Santosh Kumar Perumal <santosh.personalid@gmail.com>
2023-08-13 12:19:40 +0100

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -7,20 +7,19 @@ from airflow.models import DagBag
 
 class TestDagIntegrity(unittest.TestCase):
     LOAD_SECOND_THRESHOLD = 1
-
+    
     def setUp(self):
-      self.dagbag = DagBag(dag_folder=".", include_examples=False)
+       self.dagbag = DagBag(dag_folder=".", include_examples=False)
     
     def test_import_Dags(self):
-      print("Importing DAGs")
-      self.assertFalse(
-      len(self.dagbag.import_errors),
-      'DAG import failures. Errors: {}'.format(self.dagbag.import_errors))
-
-    if(len(self.dagbag.import_errors) == 0):
-      print("DAG Import passed")
-      print("unit tests completed")
-
+       print("Importing DAGs")
+       self.assertFalse(
+       len(self.dagbag.import_errors),
+       'DAG import failures. Errors: {}'.format(self.dagbag.import_errors))
+       if(len(self.dagbag.import_errors) == 0):
+          print("DAG Import passed")
+          print("unit tests completed")
+           
 print("Started unit testing ....")
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
 unittest.TextTestRunner(verbosity=1).run(suite)
